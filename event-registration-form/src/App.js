78f4157a4fbccafc21f0f686e20063e17bcfe6dd
Janne Hanhela <janne.hanhela@gmail.com>
2017-11-21 10:19:32 +0200

    Basic routing for the two pages

--- event-registration-form/src/App.js
+++ event-registration-form/src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+
 import './App.css';
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#/">
+            Event Registration Form
+          </a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <Link to={{ pathname: '/' }}> Registrations</Link>
+          </li>
+          <li>
+            <Link to={{ pathname: '/new' }}> New Registration</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function Form() {
+  return <p>form here</p>;
+}
+
+function List() {
+  return <p>list here</p>;
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <Navbar />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact={true} path="/" component={List} />
+          <Route path="/new" component={Form} />
+        </Switch>
       </div>
     );
   }
