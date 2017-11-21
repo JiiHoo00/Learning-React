be54a5522e18efa1b0c1ada34420213ca64bbac6
Janne Hanhela <janne.hanhela@gmail.com>
2017-11-21 10:35:45 +0200

    Basic visuals for the form

--- event-registration-form/src/components/Form.js
+++ event-registration-form/src/components/Form.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Form() {
-  return <p>form here</p>;
+class Form extends Component{
+  render() {
+    return (
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <strong>Ilmoittautumislomake</strong>
+        </div>
+        <div class="panel-body">
+          <form role="form">
+            <div class="form-group">
+              <label for="nimi">Name:</label>
+              <input type="text" class="form-control" id="nimi" placeholder="name" />
+
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" id="email" placeholder="email" />
+
+              <label for="ruoka">Food Choice:</label>
+              <select class="form-control" id="ruoka" ng-model="formdata.ruoka">
+                <option>Fish</option>
+                <option>Meat</option>
+                <option>Vegan</option>
+              </select>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" /> Going to sauna
+                </label>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-default">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default Form;
